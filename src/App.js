d3a904779c4a4212c1b89f9e4210fc3efbc39326
Rishik <56663764+Rishik-Yechuri@users.noreply.github.com>
2023-04-01 09:57:25 -0500

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {initializeApp} from "firebase/app";
+import {getAnalytics} from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // Import the functions you need from the SDKs you need
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    const firebaseConfig = {
+        apiKey: "AIzaSyCSFimHN-gPyo_aL_o-xJtpm7ZBTr14D9I",
+        authDomain: "axxesshack.firebaseapp.com",
+        projectId: "axxesshack",
+        storageBucket: "axxesshack.appspot.com",
+        messagingSenderId: "1050331285826",
+        appId: "1:1050331285826:web:07588ff14898456a38e0db",
+        measurementId: "G-SB2TRZP032"
+    };
+
+// Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+    return (
+        <div className="App">
+        <h1>Sup mate</h1>
+        </div>
+    );
 }
 
 export default App;
