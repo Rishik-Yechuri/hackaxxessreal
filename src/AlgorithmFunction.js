4e2bfedaafc6c8d71bcef81d59074c14ad032269
Shreya <shreyamekala01@gmail.com>
2023-04-01 21:41:59 -0500

    parameters + returns

--- src/AlgorithmFunction.js
+++ src/AlgorithmFunction.js
@@ -5,9 +5,10 @@ function newTasks() {
     let meditationArr = ["Do 5 mins of deep breathing exercises", "Color or draw mindfully for at least 15 minutes", "Listen to a new song on Spotify to lift your mood", "Take a nap for at least one hour today", "Do 10 minutes of yoga in the morning or before sleeping"];
     let cardioArr = ["Go on a walk in an nearby park", "Run one mile today", "Try a new dance workout on YouTube", "Play any sport outside for at least 30 minutes", "Stretch for 10 minutes before bed"];
     let journalingArr = ["Write down 3 good things you've achieved yesterday", "Write down your current thoughts on a sheet of paper", "Create a to-do list for today", "Create a schedule for this week and include important activities/events to stay organized", "Make a list of things you are thankful for"];
-    let connectedArr = ["Call/text an old friend that you haven't been in contact with recently", "Call your parents/guardians to tell them how much you appreicate them!", "Check-in with a friend and ask how they're doing", "Start small talk with a neighbor/stranger", "Make plans to go out with a friend"]
+    let connectedArr = ["Call/text an old friend that you haven't been in contact with recently", "Call your parents/guardians to tell them how much you appreicate them!", "Check-in with a friend and ask how they're doing", "Start small talk with a neighbor/stranger", "Make plans to go out with a friend"];
     let k = 0;
     let outputArr = [];
+    let categoryOutArr = [];
 
         for (let i = 0; i < arr.length; i++) {
             for (let j = 0; j < arr[i][1]; j++) {
@@ -27,15 +28,19 @@ function newTasks() {
             let k = Math.floor(Math.random()*5)
             if(j == 0) {
                 outputArr[i]= meditationArr[k];
+                categoryOutArr[i] = j;
             }
             if (j == 1) {
                 outputArr[i] = cardioArr[k];
+                categoryOutArr[i] = j;
             }
             if (j == 2) {
                 outputArr[i] = journalingArr[k];
+                categoryOutArr[i] = j;
             }
             if (j == 3) {  
                 outputArr[i] = connectedArr[k];
+                categoryOutArr[i] = j;
             }
 
             for(let l = 0; l < outputArr.length; l++) {
